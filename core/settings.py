78ae7651d8b7586816ba9733f458e275b96e273a
Rinaldito <rinalditoryanari@gmail.com>
2023-09-17 15:16:43 +0700

    no db

--- core/settings.py
+++ core/settings.py
@@ -85,14 +85,6 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST':'localhost',
-        'PORT':'3306',
-    }
 }
 
 
